50d460352a13c8bf4044036960f7fc0636144144
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2025-09-05 16:43:33 -0700

    use threadlist-sidebar

--- app/assistant.tsx
+++ app/assistant.tsx
@@ -8,7 +8,7 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
+import { ThreadListSidebar } from "@/components/assistant-ui/threadlist-sidebar";
 import { Separator } from "@/components/ui/separator";
 import {
   Breadcrumb,
@@ -26,7 +26,7 @@ export const Assistant = () => {
     <AssistantRuntimeProvider runtime={runtime}>
       <SidebarProvider>
         <div className="flex h-dvh w-full pr-0.5">
-          <AppSidebar />
+          <ThreadListSidebar />
           <SidebarInset>
             <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
               <SidebarTrigger />
